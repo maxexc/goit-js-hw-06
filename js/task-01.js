01f7fe863a448ded98bfe938ea03921b08dd7cf3
maxexc <max.exc7@gmail.com>
2022-09-17 20:31:17 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,7 +1,22 @@
 const numberOfCategories = document.querySelectorAll(".item");
 console.log("Number of categories:", numberOfCategories.length);
 
-const categoryNamesAndCounts = numberOfCategories.forEach((elem) => {
-  console.log(`Category: ${elem.querySelector("h2").textContent}`);
-  console.log(`Elements: ${elem.querySelectorAll("li").length}`);
+const titleH1 = document.querySelector("#categories");
+
+const result = numberOfCategories.forEach((elem) => {
+  const title = elem.querySelector("h2").textContent;
+  const number = elem.querySelectorAll("li").length;
+  console.log(`Category: ${title}`);
+  console.log(`Elements: ${number}`);
+  return titleH1.append(`${title}: ${number}  `);
 });
+
+// ну или так, без вывода на страницу)
+
+// const numberOfCategories = document.querySelectorAll(".item");
+// console.log("Number of categories:", numberOfCategories.length);
+
+// const categoryNamesAndCounts = numberOfCategories.forEach((elem) => {
+//   console.log(Category: ${elem.querySelector("h2").textContent});
+//   console.log(Elements: ${elem.querySelectorAll("li").length});
+// });
