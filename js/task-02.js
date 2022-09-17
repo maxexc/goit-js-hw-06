5c510dc5c4715f3562d45f08c6b64d73c323c7ab
maxexc <max.exc7@gmail.com>
2022-09-18 00:06:58 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,29 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingridientsList = document.querySelector("#ingredients");
+// console.log(ingridientsList);
+
+const ingredientsArray = ingredients.map((elem) => {
+  const itemList = document.createElement("li");
+  itemList.textContent = elem;
+  itemList.classList.add("item");
+
+  return itemList;
+});
+
+ingridientsList.append(...ingredientsArray);
+// console.log(ingridientsList);
+
+// const ingridientListEl = ingredients
+//   .map((elem) => `<li class="item">${elem}</li>`)
+//   .join("");
+
+// // console.log(ingrListEl);
+// ingridientsList.insertAdjacentHTML("beforeend", ingridientListEl);
