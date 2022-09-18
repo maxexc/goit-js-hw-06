719e94012ec0ce4bda65e35ab951477267f1f9c7
maxexc <max.exc7@gmail.com>
2022-09-19 00:53:30 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,11 @@
+const btnColorRef = document.querySelector(".change-color");
+const colorRef = document.querySelector(".color");
+const bodyRef = document.querySelector("body");
+
+btnColorRef.addEventListener("click", getRandomHexColor);
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  let color = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  bodyRef.style.backgroundColor = color;
+  colorRef.textContent = color;
 }
