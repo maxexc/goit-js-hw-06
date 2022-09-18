6218765a7f1e2c52a14dca8bf06ec91126fe0214
maxexc <max.exc7@gmail.com>
2022-09-19 00:11:11 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const formRef = document.querySelector(".login-form");
+console.log(formRef);
 
+formRef.addEventListener("submit", fillSubmit);
+
+function fillSubmit(event) {
+  event.preventDefault();
+  const { email, password } = event.currentTarget.elements;
+
+  // if (email.value === "" || password.value === "")
+  if (!email.value || !password.value) {
+    return alert("Please fill all fields!");
+  }
+  const submitData = { email: email.value, password: password.value };
+
+  console.log(submitData);
+  event.currentTarget.reset();
+}
