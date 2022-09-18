3951aa068afd3a1fb428369d1db6094c3834abbb
maxexc <max.exc7@gmail.com>
2022-09-18 23:10:08 +0300

    Update task-07.js

---
+++ js/task-07.js
@@ -0,0 +1,14 @@
+const inputRangeRef = document.querySelector('[type="range"]');
+console.log(`set value: ${inputRangeRef.value}`);
+const changeTextRef = document.querySelector("#text");
+
+inputRangeRef.addEventListener("change", fontSize);
+
+function fontSize() {
+  changeTextRef.style.fontSize = `${inputRangeRef.value}px`;
+  console.log(`fontSize: ${changeTextRef.style.fontSize}`);
+}
+
+// inputRangeRef.addEventListener("change", (event) => {
+//   changeTextRef.style.fontSize = `${event.currentTarget.value}px`;
+// });
